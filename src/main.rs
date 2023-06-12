b300af69722bdbe76300c8953d4a760921fae8c3
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2023-06-12 13:38:46 +0200

    ids

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
+//#![feature(generic_arg_infer)]
 #![cfg_attr(feature="portable_simd", feature(portable_simd))]
-use {vector::xy, image::Image};
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-	//const SIZE : xy<u32> = xy{x: 320, y: 200};
+use std::ptr::{null,null_mut};//{vector::xy, image::Image};
+#[repr(C)] pub struct CameraDescriptor {
+	id: u64,
+	r#type: u32,
+	model: [u8; 64],
+	serial: [u8; 64],
+	name: [u8; 64],
+	reserved: [u8; 448]
+}
+//impl Default for CameraDescriptor { fn default() -> Self { Self{id:0, r#type:0, model:[0;_], serial:[0;_], name: [0;_], reserved: [0:_]}}}
+impl Default for CameraDescriptor { fn default() -> Self { Self{id:0, r#type:0, model:[0;64], serial:[0;64], name: [0;64], reserved: [0;448]}}}
+// C:\Program Files\IDS\ids_peak\comfort_sdk\api\{include\ids_peak_comfort_c/ids_peak_comfort_c.h, lib/x86_64/ids_peak_comfort_c.dll}
+#[repr(C)] pub struct Camera { _data: [u8; 0], _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>}
+#[repr(C)] pub struct Frame { _data: [u8; 0], _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>}
+#[link(name="ids_peak_comfort_c", kind="raw-dylib")] extern "C" { 
+	#[link_name="peak_Library_Init"] pub fn Library_init() -> u32;
+	#[link_name="peak_CameraList_Update"] pub fn CameraList_update(len: *const usize) -> u32; 
+	#[link_name="peak_CameraList_Get"] pub fn CameraList_get(buffer: *mut CameraDescriptor, len: *mut usize) -> u32; 
+	#[link_name="peak_Camera_Open"] pub fn Camera_open(id: u64, camera: *mut *const Camera) -> u32;
+	#[link_name="peak_Acquisition_Start"] pub fn start(camera: *const Camera, number_of_frames: u32) -> u32;
+	#[link_name="peak_Acquisition_WaitForFrame"] pub fn wait_for_frame(camera: *const Camera, timeout_ms: u32, frame: *mut *const Frame) -> u32;
+}
+
+fn main() {//-> Result<(), Box<dyn std::error::Error>> {
+  unsafe{Library_init()};
+	unsafe{CameraList_update(null_mut())};
+  let mut cameras_len = 0;
+  unsafe{CameraList_get(null_mut(), &mut cameras_len as *mut _)};
+  let mut cameras = Box::from_iter(std::iter::from_fn(|| Default::default()).take(cameras_len));
+  unsafe{CameraList_get(cameras.as_mut_ptr(), &mut cameras_len as *mut _)};
+	let mut camera = null();
+	unsafe{Camera_open(cameras[0].id, &mut camera as *mut _)};
+  //peak_Camera_ResetToDefaultSettings(hCam);
+  //double currentFramerate = 0.0;  // Used in Acquisition to calculate timeout
+  //status = peak_FrameRate_GetRange(hCam, &minFramerate, &maxFramerate, &incFramerate);
+  //status = peak_FrameRate_Set(hCam, maxFramerate);
+  //status = peak_FrameRate_Get(hCam, &currentFramerate);
+  unsafe{start(camera, 1)};
+	let mut frame = null();
+	unsafe{wait_for_frame(camera, 1000, &mut frame as *mut _)};
+	//peak_Frame_Release(hCam, hFrame);
+	/*//const SIZE : xy<u32> = xy{x: 320, y: 200};
 	const SIZE : xy<u32> = xy{x: 160, y: 100};
 	if !std::env::args().any(|arg| arg.contains("ui")) {
 		let socket = std::env::args().skip(2).next().map(|address| std::net::UdpSocket::bind(address).unwrap()); // 192.168.0.105:8888
@@ -9,7 +49,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		let mut camera = cameleon::u3v::enumerate_cameras().unwrap().pop().unwrap();
 		camera.open().unwrap();
 		camera.load_context().unwrap();
-		let params_ctxt = camera.params_ctxt().unwrap();
+		//let params_ctxt = camera.params_ctxt().unwrap();
 		//dbg!(params_ctxt.ctxt);
 		let payload_rx = camera.start_streaming(1).unwrap();
 		loop {
@@ -73,7 +113,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 				let mut row = target.as_mut_ptr();
 				if min >= max { println!("{min} {max}"); return; }
 				//println!("{source_size} {} {factor}", target.size);
-				//println!("{min} {max}");
+				println!("{min} {max}");
 				for y in 0..source_size.y {
 					{
 						let mut row = row;
@@ -117,5 +157,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		ui::run(address, &mut View(std::net::UdpSocket::bind(address)?))
 	}
 	#[cfg(not(feature="ui"))] unimplemented!();
-	}
+	}*/
 }
